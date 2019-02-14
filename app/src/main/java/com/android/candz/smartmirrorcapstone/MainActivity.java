7b65a652347930248b0d322a5caaeb315bf950b9
Chris Mills <christophermills3333@gmail.com>
2019-02-13 20:09:47 -0500

    Changed MainActivity to use admin login process. No need for separate login activity. Also, take note of Code Analysis warnings. We should change dp to sp for text sizes.

--- app/src/main/java/com/android/candz/smartmirrorcapstone/MainActivity.java
+++ app/src/main/java/com/android/candz/smartmirrorcapstone/MainActivity.java
@@ -2,12 +2,39 @@ package com.android.candz.smartmirrorcapstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = findViewById(R.id.button1);
+        usernameText = findViewById(R.id.editText1);
+        passwordText = findViewById(R.id.editText2);
+
+        submitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                checkLogin(usernameText.getText().toString(), passwordText.getText().toString());
+            }
+        });
+    }
+
+    private void checkLogin(String username, String password) {
+        if (username.equals("admin") && password.equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Admin login...", Toast.LENGTH_SHORT).show();
+        }
     }
 }
