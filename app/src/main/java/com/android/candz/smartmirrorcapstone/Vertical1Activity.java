bac9dd5445397eb37ec6f381a4eb5b3b976de238
zachpnnc <zachpnnc@gmail.com>
2019-02-25 17:10:30 -0500

    Made cahnges to activity_vertical1.xml through the Vertical1Activity file.  Changed color of date and Time text and changed the background of the page to black for Mirror usage.

--- app/src/main/java/com/android/candz/smartmirrorcapstone/Vertical1Activity.java
+++ app/src/main/java/com/android/candz/smartmirrorcapstone/Vertical1Activity.java
@@ -1,19 +1,26 @@
 package com.android.candz.smartmirrorcapstone;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Vertical1Activity extends AppCompatActivity
 {
 
-    private EditText dateText;
-    private EditText timeText;
-//    private ImageView weatherIcon;
+    private TextView dateText;
+    private TextView timeText;
+    private ImageView weatherIcon;
+    boolean run = true; //set it to false if you want to stop the timer
+    Handler mHandler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +31,54 @@ public class Vertical1Activity extends AppCompatActivity
 
         dateText = findViewById(R.id.dateTextV1);
         timeText = findViewById(R.id.timeTextV1);
-        timeText.setText("Test");
+
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        dateText.setText(dateFormat.format(date));
+        setBackgroundColor();
+        timer();
+    }
+
+
+    public void timer()
+    {
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while (run)
+                {
+                    try
+                    {
+                        Thread.sleep(1000);
+                        mHandler.post(new Runnable()
+                        {
+
+                            @Override
+                            public void run()
+                            {
+                                Calendar c = Calendar.getInstance();
+                                int sec = c.get(Calendar.SECOND);
+                                int min = c.get(Calendar.MINUTE);
+                                int hour = c.get(Calendar.HOUR);
+                                timeText.setText(String.valueOf(hour) + ":" +
+                                        String.valueOf(min) + ":" + String.valueOf(sec));
+                            }
+                        });
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void setBackgroundColor()
+    {
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(0x000000);
     }
 }
