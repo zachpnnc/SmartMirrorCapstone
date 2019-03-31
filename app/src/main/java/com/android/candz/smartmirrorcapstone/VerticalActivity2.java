a5cc0cdfdb86c814c8725131f58c52cd102bcb01
Chris Mills <christophermills3333@gmail.com>
2019-03-30 20:17:10 -0400

    Added some data to a calendar event. The values were not being added to the appropriate view.

--- app/src/main/java/com/android/candz/smartmirrorcapstone/VerticalActivity2.java
+++ app/src/main/java/com/android/candz/smartmirrorcapstone/VerticalActivity2.java
@@ -14,6 +14,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,17 +31,24 @@ public final class VerticalActivity2 extends AppCompatActivity {
     private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
     private int callbackId;
-    private TextView calendar_view;
+    //private TextView calendar_view;
+    private View the_view;
+    private LinearLayout ll;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vertical2);
         callbackId = 42;
-        calendar_view = findViewById(R.id.calendar_text);
+        //calendar_view = findViewById(R.id.calendar_text);
+        //the_view = findViewById(R.id.calendar_text);
+
+        ll = findViewById(R.id.ll);
 
         addCalendar();
-        getDataFromCalendarTable();
+        addEvent(ll);
+        getDataFromEventTable(ll);
+        //getDataFromCalendarTable();
     }
 
     public void addCalendar() {
@@ -64,7 +72,7 @@ public final class VerticalActivity2 extends AppCompatActivity {
                 ContentResolver contentResolver = this.getContentResolver();
                 try {
                     contentResolver.insert(calendarUri, contentValues);
-                } catch(NullPointerException e) {
+                } catch (NullPointerException e) {
                     e.printStackTrace();
                     Toast.makeText(getApplicationContext(), "ERROR: Cannot insert data.", Toast.LENGTH_SHORT).show();
                 }
@@ -74,13 +82,13 @@ public final class VerticalActivity2 extends AppCompatActivity {
 
     static Uri asSyncAdapter(Uri uri, String account, String accountType) {
         return uri.buildUpon()
-                .appendQueryParameter(android.provider.CalendarContract.CALLER_IS_SYNCADAPTER,"true")
+                .appendQueryParameter(android.provider.CalendarContract.CALLER_IS_SYNCADAPTER, "true")
                 .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_NAME, account)
                 .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_TYPE, accountType).build();
     }
 
     public void getDataFromCalendarTable() {
-        String[] EVENT_PROJECTION = new String[] {
+        String[] EVENT_PROJECTION = new String[]{
                 CalendarContract.Calendars._ID,
                 CalendarContract.Calendars.ACCOUNT_NAME,
                 CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
@@ -95,7 +103,7 @@ public final class VerticalActivity2 extends AppCompatActivity {
                 + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
                 + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
 
-        String[] selectionArgs = new String[] {"" +
+        String[] selectionArgs = new String[]{"" +
                 "hera@example.com",     // account name
                 "com.example",          // account type
                 "hera@example.com"};    // owner account
@@ -138,5 +146,65 @@ public final class VerticalActivity2 extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void addEvent(View view) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CALENDAR}, callbackId);
+        }
+
+        ContentResolver cr = getContentResolver();
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2017, 02, 04, 9, 30);
+
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2017, 4, 4, 7, 35);
+
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events.DTSTART, beginTime.getTimeInMillis());
+        values.put(CalendarContract.Events.DTEND, endTime.getTimeInMillis());
+        values.put(CalendarContract.Events.TITLE, "Tech Stores");
+        values.put(CalendarContract.Events.DESCRIPTION, "Successful Startups");
+        values.put(CalendarContract.Events.CALENDAR_ID, 2);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, "Europe/London");
+        values.put(CalendarContract.Events.EVENT_LOCATION, "London");
+        values.put(CalendarContract.Events.GUESTS_CAN_INVITE_OTHERS, "1");
+        values.put(CalendarContract.Events.GUESTS_CAN_SEE_GUESTS, "1");
+
+        cr.insert(CalendarContract.Events.CONTENT_URI, values);
+    }
+
+    public void getDataFromEventTable(View v) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, callbackId);
+        }
+        Cursor cur = null;
+        ContentResolver cr = getContentResolver();
+
+        String[] mProjection =
+                {
+                        "_id",
+                        CalendarContract.Events.TITLE,
+                        CalendarContract.Events.EVENT_LOCATION,
+                        CalendarContract.Events.DTSTART,
+                        CalendarContract.Events.DTEND,
+                };
+
+        Uri uri = CalendarContract.Events.CONTENT_URI;
+        String selection = CalendarContract.Events.EVENT_LOCATION + " = ? ";
+        String[] selectionArgs = new String[]{"London"};
+
+        cur = cr.query(uri, mProjection, selection, selectionArgs, null);
+
+        while (cur.moveToNext()) {
+            String title = cur.getString(cur.getColumnIndex(CalendarContract.Events.TITLE));
+
+            TextView tv1 =  new TextView(this);
+            tv1.setText(title);
+            ll.addView(tv1);
+        }
+
     }
 }
