5cc344227047644d4b00ebbdff637a83455a5733
zachpnnc <zachpnnc@gmail.com>
2019-04-01 16:23:49 -0400

    Implemented loading icons into the XML, needs to be tested when conditions are more varied.

--- app/src/main/java/com/android/candz/smartmirrorcapstone/WeatherExpanded.java
+++ app/src/main/java/com/android/candz/smartmirrorcapstone/WeatherExpanded.java
@@ -2,6 +2,7 @@ package com.android.candz.smartmirrorcapstone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.JsonArray;
@@ -15,6 +16,7 @@ public class WeatherExpanded extends AppCompatActivity
 
     public TextView[] timeArray = new TextView[12];
     public TextView[] weatherArray = new TextView[12];
+    public ImageView[] iconArray = new ImageView[12];
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,12 +28,18 @@ public class WeatherExpanded extends AppCompatActivity
         String weatherData = WeatherFetch.getAPIData3Hourly();
         JsonObject jsonObject = WeatherFetch.convertJsonStringToJsonObject(weatherData);
         String[] jsonTemperatures = parseJsonForTemperatures(jsonObject);
-        String[] temperatureIcons = parseJsonForIcons(jsonObject);
 
 
         String[] adjustedTemperatures = fillInGaps(jsonTemperatures);
+        String[] temperatureIcons = parseJsonForIcons(jsonObject);
+        //weatherDataArray[0][i] contains temperature data.
+        //weatherDataArray[1][i] contains the associated iconId for that hours temperature data;
+        String[][] weatherDataArray = new String[2][40];
+        weatherDataArray[0] = adjustedTemperatures;
+        weatherDataArray[1] = temperatureIcons;
+
         //Can add in code to load in weather icon, and wind speeds / direction.
-        loadXMLinformation(adjustedTemperatures);
+        loadXMLinformation(weatherDataArray);
 
 
     }
@@ -64,6 +72,18 @@ public class WeatherExpanded extends AppCompatActivity
         weatherArray[10] = findViewById(R.id.textView23);
         weatherArray[11] = findViewById(R.id.textView24);
 
+        iconArray[0] = findViewById(R.id.iconView1);
+        iconArray[1] = findViewById(R.id.iconView2);
+        iconArray[2] = findViewById(R.id.iconView3);
+        iconArray[3] = findViewById(R.id.iconView4);
+        iconArray[4] = findViewById(R.id.iconView5);
+        iconArray[5] = findViewById(R.id.iconView6);
+        iconArray[6] = findViewById(R.id.iconView7);
+        iconArray[7] = findViewById(R.id.iconView8);
+        iconArray[8] = findViewById(R.id.iconView9);
+        iconArray[9] = findViewById(R.id.iconView10);
+        iconArray[10] = findViewById(R.id.iconView11);
+        iconArray[11] = findViewById(R.id.iconView12);
     }
 
     public String[] parseJsonForTemperatures(JsonObject jsonObject)
@@ -142,7 +162,16 @@ public class WeatherExpanded extends AppCompatActivity
         return adjustedTemperaturesString;
     }
 
-    public void loadXMLinformation(String[] temperatures)
+    public void loadXMLinformation(String[][] temperatures)
+    {
+        loadXMLTemperatures(temperatures[0]);
+
+
+        loadXMLIcons(temperatures[1]);
+    }
+
+
+    public void loadXMLTemperatures(String[] temperatures)
     {
         for (int i = 0; i < 12; i++)
         {
@@ -150,6 +179,72 @@ public class WeatherExpanded extends AppCompatActivity
             //can be difficult to implement.
             timeArray[i].setText(i + " hours from now");
             weatherArray[i].setText(temperatures[i]);
+        }
+    }
+
+    public void loadXMLIcons(String[] icons)
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            switch (icons[i])
+            {
+                case "01d":
+                    iconArray[i].setImageResource(R.drawable.a);
+                    break;
+                case "01n":
+                    iconArray[i].setImageResource(R.drawable.aa);
+                    break;
+                case "02d":
+                    iconArray[i].setImageResource(R.drawable.aaa);
+                    break;
+                case "02n":
+                    iconArray[i].setImageResource(R.drawable.aaaa);
+                    break;
+                case "03d":
+                    iconArray[i].setImageResource(R.drawable.b);
+                    break;
+                case "03n":
+                    iconArray[i].setImageResource(R.drawable.bb);
+                    break;
+                case "04d":
+                    iconArray[i].setImageResource(R.drawable.bbb);
+                    break;
+                case "04n":
+                    iconArray[i].setImageResource(R.drawable.bbbb);
+                    break;
+                case "09d":
+                    iconArray[i].setImageResource(R.drawable.c);
+                    break;
+                case "09n":
+                    iconArray[i].setImageResource(R.drawable.cc);
+                    break;
+                case "10d":
+                    iconArray[i].setImageResource(R.drawable.ccc);
+                    break;
+                case "10n":
+                    iconArray[i].setImageResource(R.drawable.cccc);
+                    break;
+                case "11d":
+                    iconArray[i].setImageResource(R.drawable.d);
+                    break;
+                case "11n":
+                    iconArray[i].setImageResource(R.drawable.dd);
+                    break;
+                case "13d":
+                    iconArray[i].setImageResource(R.drawable.ddd);
+                    break;
+                case "13n":
+                    iconArray[i].setImageResource(R.drawable.dddd);
+                    break;
+                case "50d":
+                    iconArray[i].setImageResource(R.drawable.e);
+                    break;
+                case "50n":
+                    iconArray[i].setImageResource(R.drawable.ee);
+                    break;
+                default:
+                    iconArray[i].setImageResource(R.drawable.weather_cloud);
+            }
         }
     }
 }
